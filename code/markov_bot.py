e60d8c1b603db39d6d235c93090d158eba6a0645
Brendan Brzycki <bbrzycki@uchicago.edu>
2020-04-23 14:59:55 -0700

    Update get_word_list function

--- code/markov_bot.py
+++ code/markov_bot.py
@@ -1,14 +1,15 @@
 import collections
+import numpy as np
 
 def get_word_list(input_text):
     word_list = []
     cur_word = ""
 
     for char in input_text.strip():
-        if char in " \n\t" and len(cur_word) > 0:
+        if char in " \n\t" and len(cur_word) > 0: # get rid of whitespace
             word_list.append(cur_word)
             cur_word = ""
-        elif char in ",!?.":
+        elif char in ",!?.": # treat punctuation as a token
             if len(cur_word) > 0:
                 word_list.append(cur_word)
 
@@ -21,21 +22,36 @@ def get_word_list(input_text):
 
 def main():
     filepath = "data/declaration_of_independence.txt"
+    memory_size = 2
 
     with open(filepath, "r") as file:
         input_text = file.read()
 
         word_list = get_word_list(input_text)
 
-        freq_dict = collections.defaultdict(int)
-        for tuple in (zip(word_list[:-2], word_list[1:-1], word_list[2:])):
-            freq_dict[tuple] += 1
+        ### Get counts ###
+        freq_dict = {}
+        for i in range(len(word_list)-memory_size):
+            key = tuple(word_list[i:(i+memory_size)])
+            if key in freq_dict:
+                if word_list[i+memory_size] in freq_dict[key]:
+                    freq_dict[key][word_list[i+memory_size]] += 1
+                else:
+                    freq_dict[key][word_list[i+memory_size]] = 1
+            else:
+                freq_dict[key] = {word_list[i+memory_size]: 1}
 
+        ### Get frequencies ###
         for key in freq_dict:
+            total_count = sum(freq_dict[key].values())
+            for inner_key in freq_dict[key]:
+                freq_dict[key][inner_key] /= total_count
 
+        ### Get start of sentence ###
+
+        ### Generate sentence ###
 
         print(freq_dict)
-
 
 if __name__ == "__main__":
     main()
