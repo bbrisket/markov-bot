fe16cb90d09c9a89d71cc6613eff18f80e95bebf
Brendan Brzycki <bbrzycki@uchicago.edu>
2020-04-23 14:29:44 -0700

    Add Shakespeare text data, update word_list function

--- code/markov_bot.py
+++ code/markov_bot.py
@@ -1,26 +1,38 @@
 import collections
 
+def get_word_list(input_text):
+    word_list = []
+    cur_word = ""
+
+    for char in input_text.strip():
+        if char in " \n\t" and len(cur_word) > 0:
+            word_list.append(cur_word)
+            cur_word = ""
+        elif char in ",!?.":
+            if len(cur_word) > 0:
+                word_list.append(cur_word)
+
+            word_list.append(char)
+            cur_word = ""
+        else:
+            cur_word += char
+
+    return word_list
+
 def main():
-    with open("declaration_of_independence.txt", "r") as file:
+    filepath = "data/declaration_of_independence.txt"
+
+    with open(filepath, "r") as file:
         input_text = file.read()
 
-        word_list = []
-        cur_word = ""
-        for char in input_text.strip():
-            if char in " \n\t" and len(cur_word) > 0:
-                word_list.append(cur_word)
-                cur_word = ""
-            elif char in ",!?.":
-                if len(cur_word) > 0:
-                    word_list.append(cur_word)
-                word_list.append(char)
-                cur_word = ""
-            else:
-                cur_word += char
+        word_list = get_word_list(input_text)
 
         freq_dict = collections.defaultdict(int)
         for tuple in (zip(word_list[:-2], word_list[1:-1], word_list[2:])):
             freq_dict[tuple] += 1
+
+        for key in freq_dict:
+
 
         print(freq_dict)
 
