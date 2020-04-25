e444d5852d11cad0a3c4fdbf28cd0fc477a942d2
Brendan Brzycki <bbrzycki@uchicago.edu>
2020-04-24 21:38:17 -0700

    Update sentence generation method to permit random sentence beginnings

--- code/markov_bot.py
+++ code/markov_bot.py
@@ -1,46 +1,77 @@
 import collections
 import numpy as np
 
-def get_word_list(input_text):
-    word_list = []
-    cur_word = ""
+def get_token_list(input_text):
+    token_list = []
+    cur_token = ""
 
     for char in input_text.strip():
-        if char in " \n\t": # get rid of whitespace
-            if len(cur_word) > 0:
-                word_list.append(cur_word)
-            cur_word = ""
-        elif char in ",;!?.": # treat punctuation as a token
-            if len(cur_word) > 0:
-                word_list.append(cur_word)
+        if char in " ": # get rid of spaces between tokens
+            if len(cur_token) > 0:
+                token_list.append(cur_token)
+            cur_token = ""
+        elif char in ",;!?.\n\t": # treat punctuation, decorative whitespace as a token
+            if len(cur_token) > 0:
+                token_list.append(cur_token)
 
-            word_list.append(char)
-            cur_word = ""
+            token_list.append(char)
+            cur_token = ""
         else:
-            cur_word += char
+            cur_token += char
 
-    return word_list
+    return token_list
+
+def is_valid_start(prev_char, key, mode):
+    if mode == "shakespeare":
+        return (prev_char in " \n\t" and key[0][0] != "\n")
+    elif mode == "freedom":
+        return (key[0][0].isupper())
+
+    print("Something went wrong...")
+    raise ValueError
+
+def token_list_to_string(token_list):
+    sent = token_list[0]
+    if len(token_list) > 1:
+        for token in token_list[1:]:
+            if token not in ",;!?.\n\t":
+                sent += " "
+            sent += token
+    return sent
 
 def main():
-    filepath = "data/shakespeare_sonnets.txt"
-    memory_size = 2
+    #filepath = "data/shakespeare_sonnets.txt"
+    #mode = "shakespeare"
+
+    filepath = "data/declaration_of_independence.txt"
+    mode = "freedom"
+
+    memory_size = 1
 
     with open(filepath, "r") as file:
         input_text = file.read()
 
-        word_list = get_word_list(input_text)
+        ### Get list of token
+        all_tokens_list = get_token_list(input_text)
 
-        ### Get counts ###
+        ### Get counts, potential sentence beginnings ###
         freq_dict = {}
-        for i in range(len(word_list)-memory_size):
-            key = tuple(word_list[i:(i+memory_size)])
+        sentence_heads = []
+        for i in range(len(all_tokens_list)-memory_size):
+            key = tuple(all_tokens_list[i:(i+memory_size)])
+
+            ## Collect potential sentence starts
+            if i > 0 and is_valid_start(all_tokens_list[i-1], key, mode):
+                sentence_heads.append(key)
+
+            ## Increment counts
             if key in freq_dict:
-                if word_list[i+memory_size] in freq_dict[key]:
-                    freq_dict[key][word_list[i+memory_size]] += 1
+                if all_tokens_list[i+memory_size] in freq_dict[key]:
+                    freq_dict[key][all_tokens_list[i+memory_size]] += 1
                 else:
-                    freq_dict[key][word_list[i+memory_size]] = 1
+                    freq_dict[key][all_tokens_list[i+memory_size]] = 1
             else:
-                freq_dict[key] = {word_list[i+memory_size]: 1}
+                freq_dict[key] = {all_tokens_list[i+memory_size]: 1}
 
         ### Get frequencies ###
         for key in freq_dict:
@@ -49,8 +80,8 @@ def main():
                 freq_dict[key][inner_key] /= total_count
 
         ### Get start of sentence ###
-        sent = "From fairest"
-        cur_memory = tuple(sent.split())
+        cur_memory = sentence_heads[np.random.randint(len(sentence_heads))]
+        token_list = list(cur_memory)
 
         ### Generate sentence ###
         num_iters = 0
@@ -59,20 +90,17 @@ def main():
             available_states = list(transitions.keys())
             transition_probs = [transitions[s] for s in available_states]
 
-            next_word = np.random.choice(available_states, p = transition_probs) # randomly choose next state
+            next_token = np.random.choice(available_states, p = transition_probs) # randomly choose next state
+            token_list.append(next_token)
 
-            if next_word not in ",;!?.":
-                sent += " "
-            sent += next_word
-
-            cur_memory = tuple((list(cur_memory) + [next_word])[1:]) # update value of current state
+            cur_memory = tuple((list(cur_memory) + [next_token])[1:]) # update value of current state
 
             if num_iters >= 300: # Failsafe
                 break
             num_iters += 1
 
-        print(sent)
-        #print(freq_dict)
+        sent = token_list_to_string(token_list)
+        print("\n", sent, "\n", sep = "")
 
 if __name__ == "__main__":
     main()
